0f109963849f6e331a73b350521720573914bff7
Matthijs Vos <mvos@flexyz.com>
2025-04-08 11:48:19 +0000

    added another build for cpp1 assignment

---
+++ src/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/TI-S4-DEBUG/src/Logger.cc" "CMakeFiles/app.dir/Logger.cc.o" "gcc" "CMakeFiles/app.dir/Logger.cc.o.d"
+  "/workspaces/TI-S4-DEBUG/src/main.cc" "CMakeFiles/app.dir/main.cc.o" "gcc" "CMakeFiles/app.dir/main.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
